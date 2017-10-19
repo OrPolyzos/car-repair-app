c19fe69fb6715d3f2150db222b099dc2a51d3313
VickyWip <vickywip@hotmail.com>
2017-10-19 03:21:58 +0300

    Create Constructors and Getters-Setters for the Repair Object

--- src/main/java/com/rcodingschool/carrepair/Domain/Repair.java
+++ src/main/java/com/rcodingschool/carrepair/Domain/Repair.java
@@ -48,4 +48,81 @@ public class Repair implements Serializable {
     public Repair() {
     }
 
+    public Repair(Long repairID, LocalDate repairDate, LocalTime repairTime, String repairStatus, String repairTasks,
+                  Integer repairTotalCost, Short repairTypeID, RepairType repairType, String vehicleID, Vehicle vehicle) {
+        this.repairID = repairID;
+        this.repairDate = repairDate;
+        this.repairTime = repairTime;
+        this.repairStatus = repairStatus;
+        this.repairTasks = repairTasks;
+        this.repairTotalCost = repairTotalCost;
+        this.repairTypeID = repairTypeID;
+        this.repairType = repairType;
+        this.vehicleID = vehicleID;
+        this.vehicle = vehicle;
+    }
+
+    public Long getRepairID() {
+        return repairID;
+    }
+
+    public void setRepairID(Long repairID) {
+        this.repairID = repairID;
+    }
+
+    public LocalDate getRepairDate() {
+        return repairDate;
+    }
+
+    public void setRepairDate(LocalDate repairDate) {
+        this.repairDate = repairDate;
+    }
+
+    public LocalTime getRepairTime() {
+        return repairTime;
+    }
+
+    public void setRepairTime(LocalTime repairTime) {
+        this.repairTime = repairTime;
+    }
+
+    public String getRepairStatus() {
+        return repairStatus;
+    }
+
+    public void setRepairStatus(String repairStatus) {
+        this.repairStatus = repairStatus;
+    }
+
+    public String getRepairTasks() {
+        return repairTasks;
+    }
+
+    public void setRepairTasks(String repairTasks) {
+        this.repairTasks = repairTasks;
+    }
+
+    public Integer getRepairTotalCost() {
+        return repairTotalCost;
+    }
+
+    public void setRepairTotalCost(Integer repairTotalCost) {
+        this.repairTotalCost = repairTotalCost;
+    }
+
+    public Short getRepairTypeID() {
+        return repairTypeID;
+    }
+
+    public void setRepairTypeID(Short repairTypeID) {
+        this.repairTypeID = repairTypeID;
+    }
+
+    public String getVehicleID() {
+        return vehicleID;
+    }
+
+    public void setVehicleID(String vehicleID) {
+        this.vehicleID = vehicleID;
+    }
 }
