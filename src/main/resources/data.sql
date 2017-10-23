4a17179f2a9d577dab973c5bc0ce3f25a8b7cee8
VickyWip <vickywip@hotmail.com>
2017-10-23 20:51:23 +0300

    Fix Repairs.csv/data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,26 @@
+INSERT INTO repair_types VALUES (1, 150, 'Small Service');
+INSERT INTO repair_types VALUES (2, 300, 'Great Service');
+
+LOAD DATA LOCAL INFILE 'C:\\Users\\vicky\\Desktop\\carrepair\\Users.csv'
+INTO TABLE users
+FIELDS TERMINATED BY ','
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 0 lines
+(afm,email,password,type,firstname,lastname,address_street,address_number,address_zip_code);
+
+LOAD DATA LOCAL INFILE 'C:\\Users\\vicky\\Desktop\\carrepair\\Vehicles.csv'
+INTO TABLE vehicles
+FIELDS TERMINATED BY ';'
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE  1 lines
+(vehicleid,brand,model,fuel_type,year,color,userid);
+
+LOAD DATA LOCAL INFILE 'C:\\Users\\vicky\\Desktop\\carrepair\\Repairs.csv'
+INTO TABLE repairs
+FIELDS TERMINATED BY ';'
+OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE  0 lines
+(repair_date_time,repair_status,repair_tasks,repair_total_cost,repair_typeid,vehicleid);
